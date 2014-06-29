a35910ed1e1e27a30e40ac215b910f4d4a38634a
Azzurrio <just.azzurri@gmail.com>
2014-06-29 20:22:51 +0200

    Style with foundation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+  def poster
+    "http://ia.media-imdb.com/images/M/#{poster_url}"
+  end
+
+  def imdb
+    "http://www.imdb.com/title/#{imdb_id}/"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :movies
+  resources :movies, only: [:show, :index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
