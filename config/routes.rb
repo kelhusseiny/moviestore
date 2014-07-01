1d3168664385e9c39da87308c5101cc5c4ba27fa
Azzurrio <just.azzurri@gmail.com>
2014-07-01 16:02:46 +0200

    Customize devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :movies, only: [:show, :index]
 
   # The priority is based upon order of creation: first created -> highest priority.
