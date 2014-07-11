a2b8111a671f177937046527c0271a756d9804db
Azzurrio <just.azzurri@gmail.com>
2014-07-11 16:18:07 +0200

    Minor Refactoring

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,21 +1,12 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: [:show]
-
-  # GET /movies
-  # GET /movies.json
+  
   def index
     @movies = Movie.all
   end
 
-  # GET /movies/1
-  # GET /movies/1.json
   def show
+    @movie = Movie.find(params[:id])
     @cart_action = @movie.cart_action current_user.try :id
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
 end
